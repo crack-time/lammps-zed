73ff4f5094d1ca514af76ab3c124132c1f94184a
Jiancheng Chen <crack-time@users.noreply.github.com>
2026-06-12 18:05:48 +0800

    fix: restore query files in languages/lammps/ — required by Zed for highlighting, brackets, indents

---
+++ languages/lammps/highlights.scm
@@ -0,0 +1,20 @@
+(comment) @comment
+(inline_comment) @comment
+
+(single_quote_string) @string
+(double_quote_string) @string
+
+(number) @number
+
+(variable) @variable
+
+(operator) @operator
+
+(command_name) @keyword
+
+"(" @punctuation.bracket
+")" @punctuation.bracket
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+"[" @punctuation.bracket
+"]" @punctuation.bracket
